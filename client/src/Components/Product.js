a698e72f6cf676db6546e181915778a70da6557f
jermiedomingo <8jermiedomingo@gmail.com>
2017-10-25 09:50:36 +0800

    clean up

--- client/src/Components/Product.js
+++ client/src/Components/Product.js
@@ -7,13 +7,12 @@ class Product extends Component {
   static propTypes = {
     src: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
-    price: PropTypes.string.isRequired,
-    key: PropTypes.string.isRequired
+    price: PropTypes.string.isRequired
   }
 
   render() {
     return (
-      <div className='Product' key={this.props.key}>
+      <div className='Product'>
         <img src={this.props.src} alt='product'></img>
         <div className='product-name'>{this.props.name}</div>
         <div className='product-price'>{this.props.price}</div>
